eb2ec6d3950db4a785dbdcf60ff11719876c0587
speedcubeclub <120531943+speedcubeclub@users.noreply.github.com>
2022-12-14 15:53:33 -0800

    Save

--- rank.js
+++ rank.js
@@ -2,6 +2,7 @@
 
 //必须按顺序
 let Rank_Database = [
-    ["35.432", "(example)", "Tue,Dec.13/2022"],
-    ["1,40.321", "(example)", "Tue,Dec.13/2022"]
+    ["38.253", "Colin Lonneman", "Wed,Dec.14/2022"],
+    ["50.635", "Tony Kan (阚天成)", "Wed,Dec.14/2022"],
+    ["1:32.774", "JinWen Wu", "Wed,Dec.14/2022"]
 ];
--- users.js
+++ users.js
@@ -2,7 +2,7 @@
 let User_Library =
     [
         ["TonyKan", "Ktc000488", "kantiancheng0917@gmail.com"],
-        ["ColinConneman", "Tallac04", "clonneman@elscomets.org"],
+        ["ColinLonneman", "Tallac04", "clonneman@elscomets.org"],
         ["RuiChe", "ruichang55753309", "rchenche@elscomets.org"],
         ["JinWen", "Euphorichydra82", "jwu@elscomets.org"]
     ];
