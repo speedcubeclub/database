47af729312b10d86f461f703b834c3d8007061b7
speedcubeclub <120531943+speedcubeclub@users.noreply.github.com>
2022-12-15 16:50:35 -0800

    Save

--- users.js
+++ users.js
@@ -4,5 +4,7 @@ let User_Library =
         ["TonyKan", "Ktc000488", "kantiancheng0917@gmail.com"],
         ["ColinLonneman", "Tallac04", "clonneman@elscomets.org"],
         ["RuiChe", "ruichang55753309", "rchenche@elscomets.org"],
-        ["JinWen", "Euphorichydra82", "jwu@elscomets.org"]
+        ["JinWen", "Euphorichydra82", "jwu@elscomets.org"],
+        ["JackXie", "", ""],
+        ["Sebastian", "", ""]
     ];
