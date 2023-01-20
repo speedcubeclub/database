5275bf596bdfcd9a5690e81c1d3e73fd5df3f398
speedcubeclub <120531943+speedcubeclub@users.noreply.github.com>
2023-01-19 20:50:09 -0800

    Save

--- rank.js
+++ rank.js
@@ -8,6 +8,6 @@ let Rank_Database = [
     [0, 55, 560, "JinWen Wu", "Wed,Jan.18/2023"],
     [0, 48, 332, "Sebastion", "Wed,Jan.18/2023"],
     [0, 49, 816, "Rui Che", "Wed,Jan.18/2023"],
-    [0, 49, 816, "Ian Kong", "Thur,Jan.19/2023"],
+    [0, 36, 874, "Ian Kong", "Thur,Jan.19/2023"],
     [0, 56, 188, "Jack Xie", "Thur,Jan.19/2023"]
 ];
