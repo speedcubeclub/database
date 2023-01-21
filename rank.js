caf0b43bf8abd38f1dbc35c66dbf52a468f10f7e
speedcubeclub <120531943+speedcubeclub@users.noreply.github.com>
2023-01-20 20:07:43 -0800

    Save

--- rank.js
+++ rank.js
@@ -5,9 +5,9 @@ let Rank_Database = [
     /*[分钟,秒,毫秒,选手姓名,日期]*/
     [0, 33, 498, "Tony Kan (Webmaster)", "Wed,Jan.18/2023"],
     [0, 35, 914, "Colin Lonneman", "Wed,Jan.18/2023"],
-    [0, 55, 560, "JinWen Wu", "Wed,Jan.18/2023"],
+    [0, 41, 916, "JinWen Wu", "Wed,Jan.18/2023"],
     [0, 48, 332, "Sebastion", "Wed,Jan.18/2023"],
     [0, 49, 816, "Rui Che", "Wed,Jan.18/2023"],
-    [0, 36, 874, "Ian Kong", "Thur,Jan.19/2023"],
+    [0, 34, 597, "Ian Kong", "Thur,Jan.19/2023"],
     [0, 56, 188, "Jack Xie", "Thur,Jan.19/2023"]
 ];
